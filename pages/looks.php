92f0a8223bf77ddf8629a1ba03901014aa70c42e
Matt Felten <matt.felten@dreamhost.com>
2014-03-13 22:46:19 -0700

    Remove Jenna's photos

--- pages/looks.php
+++ pages/looks.php
@@ -10,38 +10,14 @@
 		<ul class="list cf">
 			<li><span><img src="/lib/images/looks2/photo.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-7.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/photo-copy-7_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/jenna6.jpg" width="250" height="243" /></span><img src="/lib/images/looks2/jenna6_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-11.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-11_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-9.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-9_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-5.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-5_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-18.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-18_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-2.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-2_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-13.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/photo-copy-13_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/photo-copy_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-4.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/photo-copy-4_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/jenna10.jpg" width="188" height="250" /></span><img src="/lib/images/looks2/jenna10_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-17.jpg" width="186" height="250" /></span><img src="/lib/images/looks2/photo-copy-17_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-21.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-21_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-3.jpg" width="188" height="250" /></span><img src="/lib/images/looks2/photo-copy-3_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-12.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-12_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-8.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-8_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo_jenna1.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo_jenna1_thumb.jpg" width="155" height="155" /></li>
-<?/* Keep Togther */?>
-			<li><span><img src="/lib/images/looks2/photo-copy-16.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-16_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-6.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-6_thumb.jpg" width="155" height="155" /></li>
-<?/* Keep Togther */?>
-			<li><span><img src="/lib/images/looks2/photo-copy-14.jpg" width="250" height="167" /></span><img src="/lib/images/looks2/photo-copy-14_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-15.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/photo-copy-15_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-22.jpg" width="188" height="250" /></span><img src="/lib/images/looks2/photo-copy-22_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/jenna7.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/jenna7_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/jenna8.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/jenna8_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/IMG_0002.jpg" width="250" height="185" /></span><img src="/lib/images/looks2/IMG_0002_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/jeann9.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/jeann9_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-10.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-10_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/jenna11.jpg" width="188" height="250" /></span><img src="/lib/images/looks2/jenna11_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/jenna2.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/jenna2_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-20.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-20_thumb.jpg" width="155" height="155" /></li>
-			<li><span><img src="/lib/images/looks2/photo-copy-19.jpg" width="186" height="250" /></span><img src="/lib/images/looks2/photo-copy-19_thumb.jpg" width="155" height="155" /></li>
 		</ul>
 	</div>
 </div>
