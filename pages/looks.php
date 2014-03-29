eb0f29dbbecf1e7b3563faefaec9ade990233cf7
Matt Felten <matt.felten@dreamhost.com>
2014-03-29 15:45:47 -0700

    Add new Looks photos

--- pages/looks.php
+++ pages/looks.php
@@ -18,6 +18,24 @@
 			<li><span><img src="/lib/images/looks2/photo-copy-8.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-8_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-6.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-6_thumb.jpg" width="155" height="155" /></li>
 			<li><span><img src="/lib/images/looks2/photo-copy-10.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/photo-copy-10_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image1.jpg" width="184" height="247" /></span><img src="/lib/images/looks2/image1_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image2.jpg" width="249" height="249" /></span><img src="/lib/images/looks2/image2_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image3.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/image3_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image4.jpg" width="166" height="250" /></span><img src="/lib/images/looks2/image4_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image5.jpg" width="166" height="250" /></span><img src="/lib/images/looks2/image5_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image6.jpg" width="190" height="250" /></span><img src="/lib/images/looks2/image6_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image7.jpg" width="187" height="249" /></span><img src="/lib/images/looks2/image7_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image8.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/image8_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image9.jpg" width="166" height="250" /></span><img src="/lib/images/looks2/image9_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image10.jpg" width="186" height="250" /></span><img src="/lib/images/looks2/image10_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image11.jpg" width="186" height="250" /></span><img src="/lib/images/looks2/image11_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image12.jpg" width="250" height="250" /></span><img src="/lib/images/looks2/image12_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image13.jpg" width="250" height="186" /></span><img src="/lib/images/looks2/image13_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image14.jpg" width="187" height="251" /></span><img src="/lib/images/looks2/image14_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image15.jpg" width="187" height="250" /></span><img src="/lib/images/looks2/image15_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image16.jpg" width="189" height="250" /></span><img src="/lib/images/looks2/image16_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image17.jpg" width="188" height="250" /></span><img src="/lib/images/looks2/image17_thumb.jpg" width="155" height="155" /></li>
+			<li><span><img src="/lib/images/looks2/image18.jpg" width="250" height="187" /></span><img src="/lib/images/looks2/image18_thumb.jpg" width="155" height="155" /></li>
 		</ul>
 	</div>
 </div>
